65d23139de0be180b4e5e0d9de2d02c80ea8c609
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:49:33 +0100

    Added Button components

--- src/components/Buttons/buttons.js
+++ src/components/Buttons/buttons.js
@@ -1,14 +1,37 @@
 import React from "react"
 
 // Import { PrimaryButton, SecondaryButton } from "./components/Buttons/buttons.js"
-export const PrimaryButton = ({ children }) => (
-  <button className="primary">{children}</button>
+export const PrimaryButton = (to, { children }, aria) => (
+  <button className="button-primary" aria-label={aria} title={aria} type="link">
+    <a href={to}>{children}</a>
+  </button>
 )
 
-export const SecondaryButton = ({ children }) => (
-  <button className="secondary">{children}</button>
+export const SecondaryButton = (to, { children }, aria) => (
+  <button
+    className="button-secondary"
+    aria-label={aria}
+    title={aria}
+    type="link"
+  >
+    <a href={to}>{children}</a>
+  </button>
 )
 
-export const DangerButton = ({ children }) => (
-  <button className="danger">{children}</button>
+export const DangerButton = (to, { children }, aria) => (
+  <button className="button-danger" aria-label={aria} title={aria} type="link">
+    <a href={to}>{children}</a>
+  </button>
+)
+
+export const LinkButton = (to, { children }, aria) => (
+  <a
+    href={to}
+    aria-label={aria}
+    title={aria}
+    className="button-link"
+    type="button"
+  >
+    {children}
+  </a>
 )
