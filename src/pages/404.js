b5155b9f6854f36f7d4d9dd5357bcda79f56d755
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:53:01 +0100

    Added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../style/base.scss"
+
+const ErrorPage = () => (
+  <section className="error">
+    <h1>Her har det skjedd en feil.</h1>
+  </section>
+)
+
+export default ErrorPage
