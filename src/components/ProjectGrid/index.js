766510a65e34c31a2dea1afe83905638e8c0c6cb
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:51:37 +0100

    Added ProjectGrid component

---
+++ src/components/ProjectGrid/index.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import ProjectCard from "../ProjectCard/index"
+import Wrapper from "../wrapper"
+
+import "./ProjectGrid.scss"
+
+const ProjectGrid = () => (
+  <StaticQuery
+    query={graphql`
+      query PROJECTGRID_QUERY {
+        allWordpressPost {
+          edges {
+            node {
+              acf {
+                categories
+                client
+                project_title
+              }
+              slug
+              featured_media {
+                localFile {
+                  childImageSharp {
+                    fluid {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <section className="project-grid">
+        <Wrapper width={1440}>
+          {data.allWordpressPost.edges.map(item => (
+            <ProjectCard
+              slug={`/${item.node.slug}`}
+              title={item.node.acf.project_title}
+              client={item.node.acf.client}
+              categories={item.node.acf.categories}
+              img={item.node.featured_media.localFile.childImageSharp.fluid}
+            />
+          ))}
+        </Wrapper>
+      </section>
+    )}
+  />
+)
+
+export default ProjectGrid
