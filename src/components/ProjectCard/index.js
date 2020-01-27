25ab69c82359ffc6763c0503316befa1b604f8ab
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:50:48 +0100

    Added ProjectCard component

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
+import "./ProjectCard.scss"
+const ProjectCard = ({ title, client, slug, img, categories }) => (
+  <div className="project-card">
+    <Link to={slug}>
+      <Img
+        className="project--featured-media"
+        fluid={img}
+        loading="lazy"
+        alt={title}
+      />
+      <div className="project--information">
+        <h6>
+          <span>{client}</span>
+          {" — "}
+          <span>{categories}</span>
+        </h6>
+        <h1>{title}</h1>
+      </div>
+    </Link>
+  </div>
+)
+
+export default ProjectCard
