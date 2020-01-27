6f5c7c00eab83be2767095105fbc9e9de5a6c418
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:46:55 +0100

    Added different link components

---
+++ src/components/links.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "gatsby"
+// import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+// <ExternalLink to=""></ExternalLink>
+export const ExternalLink = (to, { children }, aria) => (
+  <a
+    href={to}
+    className="external-link"
+    rel="noopener noreferrer"
+    target="_blank"
+    title={aria}
+  >
+    {children}
+  </a>
+)
+
+// <InternalLink to=""></InternalLink>
+export const InternalLink = (to, { children }, aria) => (
+  <Link className="internal-link" to={to} title={aria}>
+    {children}
+  </Link>
+)
+
+/* // <AnimatedLink to=""></AnimatedLink>
+export const AnimatedLink = (to, { children }) => (
+  <AniLink to={to} cover direction="left" duration={2} bg="#ffffff">
+    {children}
+  </AniLink>
+) */
