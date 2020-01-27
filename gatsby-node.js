ddbdf9f373a2fed67a072e5a342285d35df76e59
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:49:17 +0100

    Added templates and Wordpress data-fetching

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,55 @@
  */
 
 // You can delete this file if you're not using it
+
+// Get data from WordPress
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+if (process.env.NODE_ENV === "development") {
+  process.env.GATSBY_WEBPACK_PUBLICPATH = "/"
+}
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const ProjectTemplate = path.resolve("./src/page-templates/project.js")
+  // const PageTemplate = path.resolve("./src/templates/blog-post.js")
+
+  return graphql(`
+    {
+      allWordpressPost {
+        edges {
+          node {
+            slug
+            wordpress_id
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    const Project = result.data.allWordpressPost.edges
+    Project.forEach(post => {
+      createPage({
+        path: `/${post.node.slug}`,
+        component: ProjectTemplate,
+        context: {
+          id: post.node.wordpress_id,
+        },
+      })
+    })
+
+    /* const Pages = result.data.allWordpressPage.edges
+    Pages.forEach(page => {
+      createPage({
+        path: `/${page.node.slug}`,
+        component: PageTemplate,
+        context: {
+          id: page.node.wordpress_id,
+        },
+      })
+    }) */
+  })
+}
