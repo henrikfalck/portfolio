e2bb206179a9a970b09337c39fff8b04cb1ab6fa
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:46:30 +0100

    Added SVG icons

---
+++ src/components/icons.js
@@ -0,0 +1,73 @@
+import React from "react"
+
+export const IconExternalLink = () => (
+  <div className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <title>Icon_ekstern</title>
+      <polygon points="2 4 20 4 20 22 22 22 22 2 2 2 2 4" />
+      <rect
+        x="-0.96"
+        y="11.52"
+        width="24.91"
+        height="1.96"
+        transform="translate(-5.47 11.79) rotate(-45)"
+      />
+    </svg>
+  </div>
+)
+
+export const IconArrowRight = () => (
+  <div className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <title>Icon_arrow-right</title>
+      <rect x="2" y="11" width="18" height="2" />
+      <polygon points="10.59 3.34 19.24 12 10.59 20.66 12 22.07 22.07 12 12 1.93 10.59 3.34" />
+    </svg>
+  </div>
+)
+
+export const IconArrowDown = () => (
+  <div className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <title>Icon_arrow-down</title>
+      <rect
+        x="3.04"
+        y="9.96"
+        width="18"
+        height="2"
+        transform="translate(23 -1.07) rotate(90)"
+      />
+      <polygon points="20.69 10.55 12.04 19.21 3.38 10.55 1.96 11.96 12.04 22.04 22.11 11.96 20.69 10.55" />
+    </svg>
+  </div>
+)
+
+export const IconArrowLeft = () => (
+  <div className="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <title>Icon_arrow-left</title>
+      <rect
+        x="4.07"
+        y="11"
+        width="18"
+        height="2"
+        transform="translate(26.14 24) rotate(-180)"
+      />
+      <polygon points="13.48 20.66 4.83 12 13.48 3.34 12.07 1.93 2 12 12.07 22.07 13.48 20.66" />
+    </svg>
+  </div>
+)
+
+export const IconArrowUp = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <title>Icon_arrow-up</title>
+    <rect
+      x="3.04"
+      y="12.04"
+      width="18"
+      height="2"
+      transform="translate(-1 25.07) rotate(-90)"
+    />
+    <polygon points="3.38 13.45 12.04 4.79 20.69 13.45 22.11 12.04 12.04 1.96 1.96 12.04 3.38 13.45" />
+  </svg>
+)
