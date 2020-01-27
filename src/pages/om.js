a180506c7fe710411b049f348df312d612e2c85b
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:53:33 +0100

    Added about page for future development

---
+++ src/pages/om.js
@@ -0,0 +1,17 @@
+/* import React from "react"
+import Layout from "../components/layout"
+import { ExternalLink } from "../components/links"
+import Wrapper from "../components/wrapper"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO
+      title="Om"
+      description="Henrik Falck Mentzoni er en faglært mediegrafiker som fokuserer på UI-design og front-end utvikling."
+    />
+  </Layout>
+)
+
+export default AboutPage
+ */
