1f499238e7bbe061eca84a781219a1300c41f287
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-27 14:53:12 +0100

    Added frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
+import FrontpageHero from "../components/FrontpageHero/index"
+import ProjectGrid from "../components/ProjectGrid/index"
+import SEO from "../components/seo"
+const IndexPage = () => (
+  <Layout>
+    <SEO
+      title="Prosjekter"
+      description="Henrik Falck Mentzoni er en faglært mediegrafiker som fokuserer på UI-design og front-end utvikling."
+    />
+    <FrontpageHero />
+    <ProjectGrid />
+  </Layout>
+)
 
-export default () => <div>Hello world!</div>
+export default IndexPage
