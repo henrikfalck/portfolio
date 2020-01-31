b1e196e9b2455eb33988e795a7508a2f128a91de
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-01-31 13:55:50 +0100

    Added different layout component for project pages

---
+++ src/components/layout-project.js
@@ -0,0 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Header from "./header"
+import Footer from "./footer"
+
+import "../style/base.scss"
+import "../style/projectpage.scss"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="layout project">{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
