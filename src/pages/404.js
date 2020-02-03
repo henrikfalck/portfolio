73e17b95c59707ab3dd254a2d433dd5610c58c33
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-02-03 10:16:19 +0100

    Added 404 page and styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,23 @@
 import React from "react"
-import "../style/base.scss"
+import Wrapper from "../components/wrapper"
+import { InternalPrimaryButton } from "../components/Buttons/buttons"
+import Header from "../components/header"
+import "../style/layout/_error.scss"
 
 const ErrorPage = () => (
-  <section className="error">
-    <h1>Her har det skjedd en feil.</h1>
-  </section>
+  <>
+    <Header />
+    <section className="error">
+      <Wrapper width={1440} center>
+        <h6>#404 — side ikke funnet</h6>
+        <h1 className="display">Her har det skjedd en feil.</h1>
+        <p>Dette er egentlig litt pinlig, heh...</p>
+        <InternalPrimaryButton to="/" aria="Gå tilbake til forsiden">
+          Gå tilbake til forsiden
+        </InternalPrimaryButton>
+      </Wrapper>
+    </section>
+  </>
 )
 
 export default ErrorPage
