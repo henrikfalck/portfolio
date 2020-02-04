b59dcb08b723fac6db0bb1a790e87a0ce8e55fac
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-02-04 12:26:33 +0100

    Fixed error with gatsby-plugin-transition-link and the logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,12 +29,7 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-image`,
-    {
-      resolve: `gatsby-plugin-transition-link`,
-      options: {
-        layout: require.resolve(`./src/brand-components/logo.js`),
-      },
-    },
+    `gatsby-plugin-transition-link`,
     `gsap`,
     {
       resolve: `gatsby-source-wordpress`,
@@ -75,41 +70,38 @@ module.exports = {
     //     pixelId: "pixel id here",
     //   },
     // },
-    /*
     // ==========================================
     // GOOGLE ANALYTICS
-    */
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     // The property ID; the tracking code won't be generated without it
-    //     trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
-    //     head: false,
-    //     // Setting this parameter is optional
-    //     anonymize: true,
-    //     // Setting this parameter is also optional
-    //     respectDNT: true,
-    //     // Avoids sending pageview hits from custom paths
-    //     exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     // Delays sending pageview hits on route update (in milliseconds)
-    //     pageTransitionDelay: 0,
-    //     // Enables Google Optimize using your container Id
-    //     optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-    //     // Enables Google Optimize Experiment ID
-    //     experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-    //     // Set Variation ID. 0 for original 1,2,3....
-    //     variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-    //     // Any additional optional fields
-    //     sampleRate: 5,
-    //     siteSpeedSampleRate: 10,
-    //     cookieDomain: "example.com",
-    //   },
-    // },
-    /*
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-111113846-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "henrikfalck.no",
+      },
+    },
+
     // ==========================================
     // OFFLINE FUNCTIONALITY
-    */
     // This (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
