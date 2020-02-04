dbbef05ac89ed3b27cdd16c9e603d0dfa3b7cb4d
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-02-04 12:08:46 +0100

    Removed old componentes in comments

--- src/components/ProjectContent/index.js
+++ src/components/ProjectContent/index.js
@@ -4,21 +4,6 @@ import Wrapper from "../wrapper"
 // Styles
 import "./ProjectContent.scss"
 
-/* export const ProjectText = ({ content }) => (
-  <div className="project-text">
-    <Wrapper width={768} center>
-      <p dangerouslySetInnerHTML={{ __html: content }}></p>
-    </Wrapper>
-  </div>
-)
-
-export const ProjectMedia = ({ content }) => (
-  <div
-    className="project-media"
-    dangerouslySetInnerHTML={{ __html: content }}
-  ></div>
-) */
-
 const ProjectContent = ({ children, text }) => (
   <section className="project-content">
     <Wrapper width={768} center>
