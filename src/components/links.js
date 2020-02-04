bdf015778cf360bdffa8a0612f9ef502753dedb1
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-02-04 12:09:39 +0100

    Fixed components, changed `AnimatedLink`

--- src/components/links.js
+++ src/components/links.js
@@ -3,28 +3,37 @@ import { Link } from "gatsby"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 // <ExternalLink to=""></ExternalLink>
-export const ExternalLink = (to, { children }, aria) => (
+export const ExternalLink = ({ to, children, aria }) => (
   <a
     href={to}
     className="external-link"
     rel="noopener noreferrer"
     target="_blank"
     title={aria}
+    aria-label={aria}
   >
     {children}
   </a>
 )
 
 // <InternalLink to=""></InternalLink>
-export const InternalLink = (to, { children }, aria) => (
-  <Link className="internal-link" to={to} title={aria}>
+export const InternalLink = ({ to, children, aria }) => (
+  <Link className="internal-link" to={to} title={aria} aria-label={aria}>
     {children}
   </Link>
 )
 
 // <AnimatedLink to=""></AnimatedLink>
-export const AnimatedLink = (to, { children }) => (
-  <AniLink to={to} cover direction="left" duration={1} bg="#ffe9cf">
+export const AnimatedLink = ({ to, children, aria }) => (
+  <AniLink
+    to={to}
+    title={aria}
+    aria-label={aria}
+    cover
+    direction="down"
+    duration={1.5}
+    bg="#080a09"
+  >
     {children}
   </AniLink>
 )
