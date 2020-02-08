2069a13d3f3b7d639a27b1fe04cebfbebad50a7d
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-02-08 14:34:25 +0100

    Added image component for the About page

---
+++ src/components/SelfPortrait.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const SelfPortrait = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "henrik.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fluid={data.file.childImageSharp.fluid} />
+}
+
+export default SelfPortrait
