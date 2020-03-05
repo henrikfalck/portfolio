fc047524264b71a74319a3b848c82ecece1dff5a
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-05 14:24:56 +0100

    Removed old title, added new one

--- src/components/FrontpageHero/index.js
+++ src/components/FrontpageHero/index.js
@@ -3,24 +3,9 @@ import Wrapper from "../wrapper"
 import "./FrontpageHero.scss"
 const FrontpageHero = () => (
   <section className="hero">
-    <Wrapper width={1440}>
-      <h1 className="display desktop no-motion">
-        <span>Faglært mediegrafiker</span>
-        <br />
-        <span>med fokus på UI og</span>
-        <br />
-        <span>frontend-utvikling.</span>
-      </h1>
-      <h1 className="display tablet no-motion">
-        <span>Faglært</span>
-        <br />
-        <span>mediegrafiker</span>
-        <br />
-        <span>med fokus på UI</span>
-        <br />
-        <span>og frontend-</span>
-        <br />
-        <span>utvikling.</span>
+    <Wrapper width={1920}>
+      <h1 className="display">
+        Mediegrafiker med fokus på UI og frontend-utvikling.
       </h1>
     </Wrapper>
   </section>
