93439d69c14bc3ec303376193867985418759bf4
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-05 14:29:14 +0100

    Changed component according to the new design

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,52 +1,49 @@
 import React from "react"
-import Wrapper from "../components/wrapper"
 import { ExternalLink } from "../components/links"
 
 const Footer = () => (
   <footer>
-    <Wrapper width={1440}>
-      <div className="contact">
-        <h1 className="display">Vil du ta en prat?</h1>
+    <div className="contact">
+      <h1>Vil du ta en kaffe?</h1>
+      <h5>
+        <a href="mailto:hallo@henrikfalck.no">hallo@henrikfalck.no</a>
+        <span class="call-me" role="img" aria-label="Vi sees">
+          {" "}
+          🤙
+        </span>
+      </h5>
+    </div>
+    <div className="bottom-row">
+      <div className="copyright">
+        <p>© {new Date().getFullYear()}. Ikke stjel, er du snill.</p>
+      </div>
+      <div className="social-links">
+        <ExternalLink to="https://behance.net/henrikfalck" aria="Behance">
+          Behance
+        </ExternalLink>
+        <ExternalLink to="https://dribbble.com/henrikfalck" aria="Dribbble">
+          Dribbble
+        </ExternalLink>
+        <ExternalLink to="https://github.com/henrikfalck" aria="Github">
+          Github
+        </ExternalLink>
+        <ExternalLink to="https://instagram.com/henrikfalck_" aria="Instagram">
+          Instagram
+        </ExternalLink>
+        <ExternalLink
+          to="https://www.linkedin.com/in/henrik-falck/"
+          aria="LinkedIn"
+        >
+          LinkedIn
+        </ExternalLink>
+      </div>
+      <div className="made-with">
         <p>
-          Send meg en e-post på{" "}
-          <a href="mailto:hallo@henrikfalck.no">hallo@henrikfalck.no</a>, så
-          sees vi{" "}
-          <span role="img" aria-label="Emoji">
-            🤙
-          </span>
+          Designet i Sketch, utviklet i{" "}
+          <ExternalLink to="https://www.gatsbyjs.org">GatsbyJS</ExternalLink>{" "}
         </p>
       </div>
-    </Wrapper>
-    <Wrapper width={1440}>
-      <div className="bottom-row">
-        <div className="social-links">
-          <ExternalLink to="https://behance.net/henrikfalck" aria="Behance">
-            Behance
-          </ExternalLink>
-          <ExternalLink to="https://dribbble.com/henrikfalck" aria="Dribbble">
-            Dribbble
-          </ExternalLink>
-          <ExternalLink to="https://github.com/henrikfalck" aria="Github">
-            Github
-          </ExternalLink>
-          <ExternalLink
-            to="https://instagram.com/henrikfalck_"
-            aria="Instagram"
-          >
-            Instagram
-          </ExternalLink>
-          <ExternalLink
-            to="https://www.linkedin.com/in/henrik-falck/"
-            aria="LinkedIn"
-          >
-            LinkedIn
-          </ExternalLink>
-        </div>
-        <div className="copyright">
-          <p>© {new Date().getFullYear()}. Ikke stjel, er du snill.</p>
-        </div>
-      </div>
-    </Wrapper>
+    </div>
   </footer>
 )
 
