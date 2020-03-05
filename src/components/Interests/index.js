a01c79d459e7d78661b2a08d984fe881bc58ebda
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-05 16:04:14 +0100

    Changed text

--- src/components/Interests/index.js
+++ src/components/Interests/index.js
@@ -3,10 +3,10 @@ import "./interests.scss"
 export const Music = () => (
   <section id="music">
     <h1>
-      Spillelistene mine består av Bring Me The Horizon, Dayseeker, Dunderbeist,
-      Fit For A King, Ice Nine Kills, In Flames, Kvelertak, Memphis May Fire,
-      Motionless In White, Northlane, Polaris, Slipknot, The Dogs, Void Of
-      Vision, Wage War, Watch Out Stampede og Written by Wolves.
+      Spillelistene mine inneholder mye fra Bring Me The Horizon, Dayseeker,
+      Dunderbeist, Fit For A King, Ice Nine Kills, In Flames, Kvelertak, Memphis
+      May Fire, Motionless In White, Northlane, Polaris, Slipknot, The Dogs,
+      Void Of Vision, Wage War, Watch Out Stampede og Written by Wolves.
       <p>Det er mye metalcore her altså. Interessant.</p>
     </h1>
   </section>
