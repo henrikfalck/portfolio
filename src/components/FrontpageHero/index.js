a96ee1803f6ac6dafe22ab09761d117b03cbdf67
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-06 16:02:59 +0100

    Added and/or improved aria-label

--- src/components/FrontpageHero/index.js
+++ src/components/FrontpageHero/index.js
@@ -4,7 +4,10 @@ import "./FrontpageHero.scss"
 const FrontpageHero = () => (
   <section className="hero">
     <Wrapper width={1920}>
-      <h1 className="display">
+      <h1
+        className="display"
+        aria-label="Mediegrafiker med fokus på UI-design og front end-utvikling"
+      >
         Mediegrafiker med fokus på UI og frontend-utvikling.
       </h1>
     </Wrapper>
--- src/components/footer.js
+++ src/components/footer.js
@@ -20,21 +20,33 @@ const Footer = () => (
         <p>© {new Date().getFullYear()}. Ikke stjel, er du snill.</p>
       </div>
       <div className="social-links">
-        <ExternalLink to="https://behance.net/henrikfalck" aria="Behance">
+        <ExternalLink
+          to="https://behance.net/henrikfalck"
+          aria="Lenke til Behance, åpner i nytt vindu"
+        >
           Behance
         </ExternalLink>
-        <ExternalLink to="https://dribbble.com/henrikfalck" aria="Dribbble">
+        <ExternalLink
+          to="https://dribbble.com/henrikfalck"
+          aria="Lenke til Dribbble, åpner i nytt vindu"
+        >
           Dribbble
         </ExternalLink>
-        <ExternalLink to="https://github.com/henrikfalck" aria="Github">
+        <ExternalLink
+          to="https://github.com/henrikfalck"
+          aria="Lenke til Github, åpner i nytt vindu"
+        >
           Github
         </ExternalLink>
-        <ExternalLink to="https://instagram.com/henrikfalck_" aria="Instagram">
+        <ExternalLink
+          to="https://instagram.com/henrikfalck_"
+          aria="Lenke til Instagram, åpner i nytt vindu"
+        >
           Instagram
         </ExternalLink>
         <ExternalLink
           to="https://www.linkedin.com/in/henrik-falck/"
-          aria="LinkedIn"
+          aria="Lenke til LinkedIn, åpner i nytt vindu"
         >
           LinkedIn
         </ExternalLink>
@@ -42,7 +54,12 @@ const Footer = () => (
       <div className="made-with">
         <p>
           Designet i Sketch, utviklet i{" "}
-          <ExternalLink to="https://www.gatsbyjs.org">GatsbyJS</ExternalLink>{" "}
+          <ExternalLink
+            to="https://www.gatsbyjs.org"
+            aria="Lenke til GatsbyJS, åpner i nytt vindu"
+          >
+            GatsbyJS
+          </ExternalLink>{" "}
         </p>
       </div>
     </div>
