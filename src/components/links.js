5c5be41301a07216189b9e2d9692c788e135fef0
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-09 15:09:17 +0100

    Removed AniLink and AnimatedLink

--- src/components/links.js
+++ src/components/links.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 // <ExternalLink to=""></ExternalLink>
 export const ExternalLink = ({ to, children, aria }) => (
@@ -21,19 +20,4 @@ export const InternalLink = ({ to, children, aria }) => (
   <Link className="internal-link" to={to} title={aria} aria-label={aria}>
     {children}
   </Link>
-)
-
-// <AnimatedLink to=""></AnimatedLink>
-export const AnimatedLink = ({ to, children, aria }) => (
-  <AniLink
-    to={to}
-    title={aria}
-    aria-label={aria}
-    cover
-    direction="left"
-    duration={1.5}
-    bg="#080a09"
-  >
-    {children}
-  </AniLink>
 )
