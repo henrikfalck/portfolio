05a637da4eded862adca3b3ed4b17fbd6192a190
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-10 11:20:17 +0100

    Added sal-tags and changed up bucketlist

--- src/components/Interests/index.js
+++ src/components/Interests/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./interests.scss"
 export const Music = () => (
-  <section id="music">
+  <section id="music" data-sal="fade">
     <h1>
       Spillelistene mine inneholder mye fra Bring Me The Horizon, Dayseeker,
       Dunderbeist, Fit For A King, Ice Nine Kills, In Flames, Kvelertak, Memphis
@@ -12,19 +12,21 @@ export const Music = () => (
   </section>
 )
 export const Bucketlist = () => (
-  <section id="bucketlist">
+  <section id="bucketlist" data-sal="fade">
     <h1>
       Bucketlisten min består av å{" "}
-      <span className="item">starte eget byrå</span>,{" "}
+      <span className="item">starte eget digitalbyrå</span>,{" "}
       <span className="item">få meg hund</span>,{" "}
       <span className="item">lære meg Python</span>,{" "}
+      <span className="item done">jobbe med andre byråer som frilanser</span>,{" "}
       <span className="item done">få egne visittkort</span>,{" "}
       <span className="item">flytte til Oslo</span>,{" "}
-      <span className="item done">publisere porteføljen</span>,{" "}
-      <span className="item">levere bachelor-oppgaven</span>,{" "}
-      <span className="item">reise til Skottland</span>,{" "}
+      <span className="item">flytte inn med kjæresten</span>,{" "}
+      <span className="item done">publisere porteføljen min</span>,{" "}
+      <span className="item">starte på bachelor-studiet</span>,{" "}
+      <span className="item">reise rundt i Skottland</span>,{" "}
       <span className="item">reise til Roma</span>, og{" "}
-      <span className="item">jobbe med en stor kunde</span>.
+      <span className="item">se Bring Me The Horizon live</span>.
     </h1>
   </section>
 )
