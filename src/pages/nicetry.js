576e35a5913a95fb21ca694d1f3802bb4283ff94
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-24 15:36:30 +0100

    Fucked up the branch

---
+++ src/pages/nicetry.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../components/header"
+import Wrapper from "../components/wrapper"
+
+const HackerPage = () => (
+  <>
+    <Header />
+    <section id="hacks">
+      <Wrapper>
+        <h1>Nice try.</h1>
+      </Wrapper>
+    </section>
+  </>
+)
+
+export default HackerPage
