a44071a4639f4a4b82e604235eff7e4f6d935e11
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-03-24 15:25:47 +0100

    Added page for attempted hacks

---
+++ src/pages/nicetry.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Wrapper from "../components/wrapper"
+import Header from "../components/header"
+
+const HackerPage = () => (
+  <>
+    <Header />
+    <section id="hacks">
+      <Wrapper>
+        <h1 className="display">Nice try.</h1>
+      </Wrapper>
+    </section>
+  </>
+)
+
+export default HackerPage
