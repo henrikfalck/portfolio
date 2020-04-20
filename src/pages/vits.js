27ca77a304a3b2da98dbdf83a4c8d1e2e19caa98
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-04-20 15:38:08 +0200

    Removed Layout and joke prop, added Header

--- src/pages/vits.js
+++ src/pages/vits.js
@@ -1,15 +1,16 @@
 import React from "react"
-import Layout from "../components/layout"
+import Header from "../components/header"
 import SEO from "../components/seo"
 import Joke from "../components/Joke"
 
-const JokePage = ({ joke }) => (
-  <Layout>
+const JokePage = () => (
+  <>
+    <Header />
     <SEO title="Vits" description="En tilfeldig designer-vits" />
     <section className="joke-container">
       <Joke />
     </section>
-  </Layout>
+  </>
 )
 
 export default JokePage
