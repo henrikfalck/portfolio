60e203f1ae993bd32b708de4bb527655d7aa838b
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-04-20 15:37:29 +0200

    Updated jokes

--- src/components/Joke/index.js
+++ src/components/Joke/index.js
@@ -2,15 +2,18 @@ import React from "react"
 
 function Joke() {
   const jokeArray = [
-    "Why does UX designers like forks? They're great stakeholders.",
     "And then the client said, 'Don't use that tone with me'!",
     "What is a video game art designer's favorite soft drink? Sprite.",
     "How many graphic designers does it take to change a light bulb? Does it have to be a light bulb? Because I had this other idea.",
     "What is a UX designers favorite exercise? Sprints.",
-    "Website is done! False. A website is never done.",
-    "Photoshop summer body goals: lose 50 layers.",
+    "The website is done! False. A website is never done.",
+    "Summer goals: lose 50 layers.",
     "Your x-ray showed a broken rib, but we fixed it with Photoshop.",
-    "Practice safe design: Use a concept.",
+    "What type of attack can kill the most typographers? Arial attacks.",
+    "Here's a list of changes for your design. You can find them in the attached .txt file",
+    "Get with the times, new roman",
+    "Oh, crop",
+    "Make the logo bigger.",
   ]
 
   const randomJoke = jokeArray[Math.floor(Math.random() * jokeArray.length)]
