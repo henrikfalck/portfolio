d486486dbdb72e8fce2f2d2a7a19201ab45fbb16
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-05-09 12:17:13 +0200

    Added RepoButton

---
+++ src/components/Buttons/index.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Github } from "../icons"
+import "./buttons.scss"
+
+// Import { PrimaryButton, SecondaryButton } from "./components/Buttons/buttons.js"
+export const PrimaryButton = ({ to, children, aria }) => (
+  <button className="button-primary" aria-label={aria} title={aria} type="link">
+    <a href={to}>{children}</a>
+  </button>
+)
+
+export const SecondaryButton = ({ to, children, aria }) => (
+  <button
+    className="button-secondary"
+    aria-label={aria}
+    title={aria}
+    type="link"
+  >
+    <a href={to}>{children}</a>
+  </button>
+)
+
+export const DangerButton = ({ to, children, aria }) => (
+  <button className="button-danger" aria-label={aria} title={aria} type="link">
+    <a href={to}>{children}</a>
+  </button>
+)
+
+export const LinkButton = ({ to, children, aria }) => (
+  <a
+    href={to}
+    aria-label={aria}
+    title={aria}
+    className="button-link"
+    type="button"
+  >
+    {children}
+  </a>
+)
+
+export const InternalPrimaryButton = ({ to, children, aria }) => (
+  <Link to={to}>
+    <button
+      className="button-primary"
+      aria-label={aria}
+      title={aria}
+      type="link"
+    >
+      {children}
+    </button>
+  </Link>
+)
+
+export const RepoButton = ({ repo }) => (
+  <a href={repo}>
+    <button className="button-primary button-repo">
+      <Github />
+      Gå til repoet
+    </button>
+  </a>
+)
