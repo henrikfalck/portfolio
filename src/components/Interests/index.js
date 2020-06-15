db6aa60faf4531ddf22d50e31bde4c1dbe459c80
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-06-15 15:12:23 +0200

    Updated music and bucketlist

--- src/components/Interests/index.js
+++ src/components/Interests/index.js
@@ -4,10 +4,9 @@ export const Music = () => (
   <section id="music">
     <h2 data-sal="fade">
       Spillelistene mine inneholder mye fra Bring Me The Horizon, Bury Tomorrow,
-      Dayseeker, Dunderbeist, Fit For A King, Ice Nine Kills, In Flames,
-      Kvelertak, Memphis May Fire, Motionless In White, Northlane, Polaris,
-      Slipknot, The Dogs, Void Of Vision, Wage War, Watch Out Stampede og
-      Written by Wolves.
+      Fit For A King, Ice Nine Kills, In Flames, Kvelertak, Memphis May Fire,
+      Northlane, Polaris, The Dogs, Void Of Vision, Wage War, Watch Out Stampede
+      og Written by Wolves.
     </h2>
   </section>
 )
@@ -16,7 +15,6 @@ export const Bucketlist = () => (
     <h2 data-sal="fade">
       Noe av det jeg vil oppnå her i livet er å{" "}
       <span className="item">starte eget digitalbyrå</span>,{" "}
-      <span className="item">bungee jumping fra en (meget) høy bro</span>,{" "}
       <span className="item">lære meg Python</span>,{" "}
       <span className="item done">jobbe med andre byråer som frilanser</span>,{" "}
       <span className="item done">publisere porteføljen min</span>,{" "}
